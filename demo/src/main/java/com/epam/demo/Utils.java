c4f04bb2591872544e2fca2a54e1f04dd17c4a7b
EPAM\Ajitha_Nalluri <Ajitha_Nalluri@epam.com>
2023-01-24 19:52:12 +0530

    Gradle build tool practice

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,28 @@
 package com.epam.demo;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        int isAll = 0;
+        for (String s : args) {
+            if(StringUtils.isBlank(s) || !org.apache.commons.lang3.StringUtils.isNumeric(s)) {
+                return false;
+            }
+        }
+        try {
+            List<Integer> integerArgs = args.stream().map(Integer::parseInt).collect(Collectors.toList());
+            for (int i : integerArgs) {
+                if (i < 0) {
+                    isAll += 1;
+                }
+            }
+            return isAll == 0;
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Given String is incompatible with Integer");
+        }
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,8 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if(org.apache.commons.lang3.StringUtils.isBlank(str))
+            return false;
+        return org.apache.commons.lang3.StringUtils.isNumeric(str) && Integer.parseInt(str) > 0 && Integer.parseInt(str) % 2 == 0;
     }
 }
